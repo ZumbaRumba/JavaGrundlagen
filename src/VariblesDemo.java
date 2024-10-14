318e69aa2a15772e294a8c673500729a21d02c33
ZumbaRumba <zumbarumba@example.com>
2024-10-14 14:37:02 +0200

    VariblesDemo

---
+++ src/VariblesDemo.java
@@ -0,0 +1,12 @@
+public class VariblesDemo {
+    public static void main(String[] args) {
+
+        int number = 10;
+        String name = "Java";
+        double wertDouble = 10;
+
+        System.out.println("Number: " + number);
+        System.out.println("Name: " + name);
+        System.out.println("Double Wert: " + wertDouble);
+    }
+}
